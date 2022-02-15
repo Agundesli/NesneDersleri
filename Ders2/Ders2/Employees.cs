cf254e253fea013bd9186d2b8d0ba5130b06972f
Abdullah <agundesli06@gmail.com>
2022-02-15 19:28:05 +0300

    Constructor

---
+++ Ders2/Ders2/Employees.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ders2
+{
+    class Employees
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Address { get; set; }
+
+        public Employees(string name, string address):this(1,name,address)//this ile yaparsak içeriği yazmamıza gerek kalmaz.
+        {
+
+        }
+
+        public Employees(int id, string name, string address)
+        {
+            EmpId = id;
+            EmpName = name;
+            Address = address;
+        }
+    }
+}
---
+++ Ders2/Ders2/Try.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ders2
+{
+    class Try
+    {
+        const double pi = 3.1415;
+        readonly double katsayi = 1.5;
+
+
+        public Try()
+        {
+            pi = 2.5;
+            katsayi = 1.1;
+        }
+
+        public void Islem()
+        {
+            katsayi=5.6; //Burada metotda değiştirmeme izin vermiyor.
+        }
+
+        //6-readonly => değiştirebilirim o da ctor tek değiştirilecek yer ctordur
+        //7-const => asla değiştiremem
+    }
+}
