4e8e130d3cd129d346630bd0ddb069595e39944a
Abdullah <agundesli06@gmail.com>
2022-02-21 19:22:20 +0300

    Encapsülation

---
+++ Ders1/Encapsülation/Encapsülation.sql
@@ -0,0 +1,22 @@
+﻿Nesne yönelimli programlamanın ilk prensibi kapsülleme (encapsulation) olarak adlandırılır. 
+Bu özellik, dilin nesne kullanıcısından gereksiz uygulama ayrıntılarını saklar. 
+Oluşturulan bir sınıf içerisinde kullanıcının işlemlerini daha kolay gerçekleştirebilmesi için bazı işlemler 
+birleştirilerek tek bir işlem gibi gösterilir. Bu birleştirme işlemine kapsülleme denir.
+
+Erişim belirteçleri (access modifier) sayesinde kapsülleme çok daha kolay yapılmaktadır. 
+Erişim belirteçleri, oluşturulan sınıf veya sınıf içindeki elemanların erişim seviyelerini belirlemek için kullanılan 
+anahtar kelimeler grubuna verilen isimdir. 
+Metotlar ve değişkenler bir anahtar sözcük ile önceden bellirlenen sınırlar dahilinde kullanılabilir. 
+Bu anahtar kelimeler şu şekilde sıralanabilir.
+
+public: Sistemdeki bütün sınıfların erişebilmesini sağlar. Yalnızca aynı proje içinden değil, diğer projelerden de erişim sağlanabilir.
+
+private: Bir "özellik (property)"in veya "metod"un sadece tanımlandığı sınıftan erişilebilmesini sağlar. 
+Oluşturulan sınıf veya yapıların "public" olması açık bir şekilde belirtilmez ise, derleyici tarafından "private" olarak belirlenir.
+
+internal: Aynı derleyici (assembly) içinde bulunan tüm sınıflardan erişim sağlanır. 
+
+protected: Sadece tanımlandığı sınıfın içinde ve o sınıftan türetilmiş diğer sınıfların içinde erişilebilir.
+
+Kapsülleme "private" değişkenlerin metotlar gibi kullanılmasına yardımcı olur. Okuma (Read Only) işleminin yanısıra 
+okuma - yazma (read - write) işleminin yapılmasını sağlar.
---
+++ Ders1/Encapsülation/Encapsülation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Encapsülation
+{
+    class Program
+    {
+        //Id, Ad, OkulNo ve HarcUcreti alanlarından oluşan Ogrenci adlı bir sınıfın; HarcUcreti alanı kapsüllenip, birisi nesneye değer atamasını kontrol eden
+        //SetHarcUcreti metodu, diğeri de atanan değeri geriye döndüren GetHarcUcreti metodlarıyla kapsülleme uygulaması şu şekilde yapılır:
+        static void Main(string[] args)
+        {
+            double value;
+            Console.WriteLine("Harç Ücreti Giriniz: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Id = 1;
+            ogrenci.Ad="Abdullah Usame Gündeşli";
+            ogrenci.OkulNo = "1281";
+            ogrenci.SetHarcUcreti(value);
+            Console.WriteLine(ogrenci.Id);
+            Console.WriteLine(ogrenci.Ad);
+            Console.WriteLine(ogrenci.OkulNo);
+            Console.WriteLine(ogrenci.GetHarcUcreti());
+        }
+    }
+    class Ogrenci
+    {
+        public int Id;
+        public string Ad;
+        public string OkulNo;
+        private double HarcUcreti;//Harc Ücreti Alanını Kapsülleyeceğim
+        public void SetHarcUcreti(double value)
+        {
+            if (value>0)//Set metoduyla verilen değerin uygun olması durumunda HarcUcreti alanına atama yapılır
+            {
+                HarcUcreti = value;
+            }
+            else
+            {
+                Console.WriteLine("Girilen Tutar Geçersiz...");
+            }
+            
+        }
+        public double GetHarcUcreti()
+        {
+            return HarcUcreti;
+        }
+    }
+}
