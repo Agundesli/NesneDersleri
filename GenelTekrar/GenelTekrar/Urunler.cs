27bc04caa76aaa2e20d8508fa9a4b717f28fbaa4
Abdullah <agundesli06@gmail.com>
2022-02-21 22:30:55 +0300

    GenelTekrar

---
+++ GenelTekrar/GenelTekrar/Urunler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenelTekrar
+{
+    abstract class Urunler:IEnumerable,IUrunler
+    {
+        public int UrunId { get; set; }
+        public string UrunAd { get; set; }
+        public double Fiyat { get; set; }
+        public void Add()
+        {
+            Console.WriteLine("Urun Eklendi...");
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+        }
+
+        public void Listele()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
