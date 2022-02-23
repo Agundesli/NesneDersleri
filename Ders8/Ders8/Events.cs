137a34a09ced4449b993f11d283f1aec64867921
Agundesli <87178579+Agundesli@users.noreply.github.com>
2022-02-23 13:13:13 +0300

    Delege/Events

---
+++ Ders8/Ders8/Events.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ders8
+{
+    class Events
+    {
+        //1) Temsilciye ihtiuaç var. Bizde yazabiliriz var olan temsilciyide kullanabiliriz.
+        //2) İlgili temsilciden event tanımlanır.
+        //3) Olayı tetikleyecek mekanizma kurulur.
+        //4) Class oluşturulur
+        //5) Olaya bir metot bağlanır. Nasıl?
+        //6) Çalıştır.
+        delegate void Temsil();
+    }
+    class Buton
+    {
+        public event Temsil Tikla;
+
+        public void Calistir()
+        {
+            if (Tikla!=null)
+            {
+                Tikla();
+            }
+        }
+
+    }
+}
--- Ders8/Ders8/Program.cs
+++ Ders8/Ders8/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Ders8
 {
+    public delegate void DLG();
     delegate void Temsil();
     class Program
     {
@@ -14,7 +16,7 @@ namespace Ders8
         {
             foreach (var s in sayilar)
             {
-                Console.WriteLine(s+" ");
+                Console.WriteLine(s + " ");
             }
         }
         delegate int Temsilci();
@@ -33,43 +35,106 @@ namespace Ders8
 
         static void Main(string[] args)
         {
-            Temsilci temsilci = new Temsilci(MetotA);
-            temsilci += new Temsilci(MetotB);
-            temsilci += MetotC;
-            temsilci += MetotB;
-            Console.WriteLine(temsilci());
+            Isıtıcı ısıtıcı = new Isıtıcı();
+            ısıtıcı.Kabarcik += Isıtıcı_Kabarcik;
+            ısıtıcı.SesCikar += Isıtıcı_SesCikar;
+            ısıtıcı.Fokurda += Isıtıcı_Fokurda;
+            ısıtıcı.Calistir();
 
-            
+           // Temsilci temsilci = new Temsilci(MetotA);
+           // temsilci += new Temsilci(MetotB);
+           // temsilci += MetotC;
+           // temsilci += MetotB;
+           // Console.WriteLine(temsilci());
+
+           // List<int> sayilar = new List<int>() { 4, 5, 6, 7, 8, 9 };
+           // for (int i = 0; i < sayilar.Count; i++)
+           // {
+           //     if (sayilar[i] % 2 == 1)
+           //     {
+           //         Console.Write(sayilar[i] + " ");
+           //     }
+           // }
+           // Console.WriteLine();
+           // //yöntem1
+           // Predicate<int> p = new Predicate<int>(TekSayiMi);
+           // Yaz(sayilar.FindAll(p));
+           // //yöntem2
+           // Yaz(sayilar.FindAll(TekSayiMi));
+           // //Anonim metot
+           // Temsil t = delegate ()
+           //  {
+           //      Console.WriteLine("Anonim Metot");
+           //  };
+           // t();
+           // //yöntem3
+           // Yaz(sayilar.FindAll(delegate (int s)
+           //{
+           //    return s % 2 == 1;
+           //}));
+           // //yöntem4
+           // Console.WriteLine("---------------------------------------------");
+           // //Yaz(sayilar.FindAll(s => s % 2 == 1));
 
 
-            List<int> sayilar = new List<int>() { 4, 5, 6, 7, 8, 9 };
-            for (int i = 0; i < sayilar.Count; i++)
+            //Events: Aslında bir pattern'dir.
+            //Amacı:Mekanızmayı kur ve çalışmasını izle.
+            //Nasıl çalıacak? Bu mekanızmayı delege ile kuruyoruz. Evenrler class memberlarıdır
+            //Console.WriteLine("--------------------------------------------------------------");
+            //Buton buton = new Buton();
+            //buton.Tikla += Buton_Tikla;
+            //buton.Calistir();
+        }
+
+        private static void Isıtıcı_Fokurda()
+        {
+            Console.WriteLine("fokur");
+        }
+
+        private static void Isıtıcı_SesCikar()
+        {
+            Console.WriteLine("sssssssssssssssssssssssssss");
+        }
+
+        private static void Isıtıcı_Kabarcik()
+        {
+            Console.WriteLine("ooooooooooooooooooooooooooooooooooo");
+        }
+
+        private static void Buton_Tikla()
+        {
+            Console.WriteLine("Butona Tıklandı...");
+        }
+    }
+    public class Isıtıcı
+    {
+        public int Isi { get; set; }
+        public event DLG Kabarcik;
+        public event DLG SesCikar;
+        public event DLG Fokurda;
+        public void Calistir()
+        {
+            for (int i = 0; i < 120; i++)
             {
-                if (sayilar[i]%2==1)
+                Thread.Sleep(50);
+                Isi++;
+                Console.WriteLine(i);
+                if (Isi >= 30 && Isi < 50 && Kabarcik != null) 
                 {
-                    Console.Write(sayilar[i]+" ");
+                    Kabarcik();
+                }
+                else if (Isi >= 50 && Isi < 110 && SesCikar != null)
+                {
+                    SesCikar();
+                }
+                else if (Isi >= 110 && Isi < 120 && Fokurda != null) 
+                {
+                    Fokurda();
                 }
             }
-            Console.WriteLine();
-            //yöntem1
-            Predicate<int> p = new Predicate<int>(TekSayiMi);
-            Yaz(sayilar.FindAll(p));
-            //yöntem2
-            Yaz(sayilar.FindAll(TekSayiMi));
-            //Anonim metot
-            Temsil t = delegate ()
-             {
-                 Console.WriteLine("Anonim Metot");
-             };
-            t();
-            //yöntem3
-            Yaz(sayilar.FindAll( delegate (int s)
-            {
-                return s % 2 == 1;
-            }));
-            //yöntem4
-            Console.WriteLine("---------------------------------------------");
-            Yaz(sayilar.FindAll(s=>s%2==1));
         }
     }
 }
+
+    
+
