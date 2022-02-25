c03d8d060283043c8afcdad35954f38dc1fe3d5a
Agundesli <87178579+Agundesli@users.noreply.github.com>
2022-02-25 10:00:22 +0300

    Genel

--- Ders9/Ders9/Program.cs
+++ Ders9/Ders9/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using Genel;
 namespace Ders9
 {
     class Program
     {
         static void Main(string[] args)
         {
-           var sayi = 
+            Cebir cebir = new Cebir();
+
+            Console.WriteLine(cebir.Kare(4)); ;
+            Console.WriteLine(cebir.KupAl(cebir.Kare(4)));
         }
     }
 }
